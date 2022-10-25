08065dc447eacca56b130feddafc4f79f27a2074
Michał Żmudzin <istredd@gmail.com>
2022-10-25 13:01:51 +0200

    first commit

---
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,70 @@
+package pl.coderslab;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TaskManager {
+
+    public static void readFromFile(String fileName) {
+
+
+        Path filePath = Paths.get(fileName);
+        StringBuilder sb = new StringBuilder();
+        try {
+            for (String line : Files.readAllLines(filePath)){
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("""
+                wybierz właściwą opcję
+                add
+                remove
+                list
+                exit
+                """);
+        String input = "";
+
+        while (!input.equals("exit")) {
+            input = scan.nextLine();
+            sb.append(input).append(" ");
+            System.out.println("""
+                    wybierz właściwą opcję
+                    add
+                    remove
+                    list
+                    exit
+                    """);
+            sb.delete(sb.length()-5,sb.length() );
+        }
+
+
+
+        String text = sb.toString();
+        Path path1 = Paths.get(fileName);
+        List<String> outList = new ArrayList<>();
+
+        outList.add(text);
+
+        try {
+            Files.write(path1, outList);
+        } catch (IOException ex) {
+            System.out.println("Nie można zapisać pliku.");
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        readFromFile("tasks.csv");
+    }
+}
